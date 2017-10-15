97a678b323580b01c4632af64c358e5def39e749
CHGH <r.sotnichenko@yandex.ru>
2017-10-15 20:45:33 +0300

    degenerate objects

--- src/test/java/ValuteTest.java
+++ src/test/java/ValuteTest.java
@@ -1,14 +1,17 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class ValuteTest
 {
+    // degenerate objects
     @Test
     public void testMultiplication()
     {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(five.amount, 10);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
