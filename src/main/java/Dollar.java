9c0dac148589a56da130f583b3a29bc7e9e5ba32
CHGH <r.sotnichenko@yandex.ru>
2017-10-15 20:56:24 +0300

    equality for all

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -7,6 +7,13 @@ class Dollar
         this.amount = amount;
     }
 
+    @Override
+    public boolean equals(Object obj)
+    {
+        Dollar dollar = (Dollar) obj;
+        return amount == dollar.amount;
+    }
+
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
